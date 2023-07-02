22c5af68f262f45a7cdd158176b95c6be9a48eff
addambenson <addam.benson@gmail.com>
2023-07-01 19:47:03 -0500

    rolling back main

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -3,7 +3,6 @@ import Home from "./components/Home";
 import SignIn from "./components/SignIn";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavMenu from "./components/NavMenu";
-import RegisterForm from "./components/register/Register";
 import UserProfile from "./components/userProfile/UserProfile";
 function App() {
   return (
@@ -14,8 +13,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/SignIn" element={<SignIn />} />
-            <Route path="/Register" element={<RegisterForm />} />
-            <Route path="/UserProfile" element={<UserProfile />} />
+            <Route path="/User" element={<UserProfile />} />
           </Routes>
         </div>
       </Router>
--- ClientApp/src/components/userProfile/UserProfile.jsx
+++ ClientApp/src/components/userProfile/UserProfile.jsx
@@ -1,15 +1,34 @@
 import React from "react";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Row, Col, Button, Table } from "react-bootstrap";
 
 const UserProfile = () => {
   return (
     <>
-      <Container className="m-5">
-        <Row>
-          <Col
-            md={4}
-            className="border border rounded border-primary p-3 text-center"
-          >
+      <Container fluid className="d-flex">
+        <Col md={3}>
+          <h2>Friends</h2>
+          <Table className="border striped">
+            <thead className="bg-secondary">
+              <td>Photo</td>
+              <td>Name</td>
+              <td>Common Interest</td>
+            </thead>
+            <tr>
+              <td>
+                <img
+                  src="https://randomuser.me/api/portraits/men/3.jpg"
+                  alt="..."
+                  className="img-fluid rounded-circle"
+                />
+              </td>
+              <td>Sean Combs</td>
+            </tr>
+          </Table>
+        </Col>
+        <Col></Col>
+        <Col>
+          <Row>
+            {/* <Col className="border border rounded border-success p-3 text-center me-2"> */}
             <h2 className="mb-3">John Snuffy</h2>
             <img
               src="https://randomuser.me/api/portraits/men/7.jpg"
@@ -23,8 +42,9 @@ const UserProfile = () => {
             <Button type="submit" className="mt-2">
               Logout
             </Button>
-          </Col>
-        </Row>
+            {/* </Col> */}
+          </Row>
+        </Col>
       </Container>
     </>
   );
